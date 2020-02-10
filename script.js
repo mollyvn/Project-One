37aecd59c7e2af6b3a6a1e4d16358f87484b9c0d
Steffield <steffilebby@gmail.com>
2020-02-10 14:03:00 -0500

    candidate and polling info works now

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
 //address needs to be turned into lat and long to create a pin
 //polling place data needs to be pulled to create the pin
-//the polling.html has a bug and doesnt show the results but it runs correctly and correct API info is in console.log
-//I think having Bootstrap and Bulma messes up the style?!?
+//I think having Bootstrap and Bulma messes up the style, is that possible?!?
 
 $(document).ready(function() {
     //=============================================================================
@@ -112,23 +111,29 @@ function pollingQuery (queryURLVoterInfo){
     //put into HTML
     var electionSection=$('<div>');
     electionSection.addClass("well");
-    electionSection.attr('data-election', 'elWell');
+    electionSection.attr('id', 'elWell');
     $('#elecSection').append(electionSection);
 
 
-    //Attach content to approp well
-    $("#elWell").append("<h2>Election: "+ response.election.name+"</h2>");
-    $("#elWell").append("<h2>Election Day: "+ response.election.electionDay+"</h2>");
+    //Attach election content to approp section
+    $("#elWell").append("<h2>Election: <br>"+ response.election.name+"</h2><br>");
+    $("#elWell").append("<h2>Election Day: <br>"+ response.election.electionDay+"</h2>");
+
+    //data for polling places
 
     for (var p=0; p<response.pollingLocations.length; p++){
         var locationSection=$("<div>");
         locationSection.addClass("well");
-        locationSection.attr('data-poll', 'pollWell-'+p );
-        $("#elecSection").append(locationSection);
+        locationSection.attr('id', 'pollWell-'+p );
+        $("#pollSection").append(locationSection);
 
         //attach the content to well
-        $("#pollWell-"+p).append("<h3>Location : "+response.pollingLocations[p].address.locationName+ "<br>"+ response.pollingLocations[p].address.line1+", " + response.pollingLocations[p].address.city+", "+ response.pollingLocations[p].address.state+", "+ response.pollingLocations[p].address.zip+"</h3>");
-        $("#pollWell-"+p).append("<h3>Polling Hours: "+response.pollingLocations[p].pollingHours+"</h3>");
+        $("#pollWell-"+p).append("<h3>Location : <br>"+response.pollingLocations[p].address.locationName + 
+            "<br>"+ response.pollingLocations[p].address.line1 + ", " 
+            + response.pollingLocations[p].address.city + ", "
+            + response.pollingLocations[p].address.state + ", "
+            + response.pollingLocations[p].address.zip+"</h3><br>");
+        $("#pollWell-"+p).append("<h3>Polling Hours: <br>"+response.pollingLocations[p].pollingHours+"</h3>");
         
         //console log bc some appending issue, but data shows correctly in console
         console.log(response.election.name);
@@ -138,15 +143,36 @@ function pollingQuery (queryURLVoterInfo){
         }
 
         //add contestants when appending works- for now only console log test
+        for (var c=0; c<response.contests.length; c++){
+            var candidateSection=$("<div>");
+            candidateSection.addClass("well");
+            candidateSection.attr('id', 'candWell-'+c );
+            $("#candidateSection").append(candidateSection);
+    
+            //attach the content to well
+            $("#candWell-"+c).append("<h3>Ballot Placement: "+response.contests[c].ballotPlacement+ "</h3>");
+            $("#candWell-"+c).append("<h3>Office : "+response.contests[c].office + "</h3>");
+            $("#candWell-"+c).append("<h3>Candidate: "+response.contests[c].candidates[0].name+ "</h3>");
+            $("#candWell-"+c).append("<h3>Party: "+response.contests[c].candidates[0].party+ "</h3><hr>");
+            
+
+            // if(response.contests[c].candidates[0].party==="GREEN"){
+            //     $(".well").css("background-color", "#2ecc40");
+            // } else if (response.contests[c].candidates[0].party==="LIBERTARIAN"){
+            //     $(".well").css("background-color", "#ffdc00");
+            // }else if (response.contests[c].candidates[0].party==="DEMOCRATIC"){
+            //     $(".well").css("background-color", "#7FDBFF");
+            // } else if(response.contests[c].candidates[0].party==="REPUBLICAN"){
+            //     $(".well").css("background-color", "#FF4136");
+            // }  else {
+            //     $(".well").css("background-color", "none");
+            // }
+        }
+
        
         console.log(response.contests.length);
         console.log(response.contests[0].candidates[0]);
-        // for(var c=0; response.contests.length;c++){
-        //     var contestSection =$("<div>");
-        //     conttestsection.addClass("well)
-        // }
-    
-    // }
+  
 
     });
 }
@@ -240,7 +266,7 @@ function runRepresentativeQuery(queryURLRepresentatives){
     
 //     });
     
-}
+// }
   
 
 //======================================
