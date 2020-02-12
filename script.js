9f5b22029f93dc4a3e1dea8a4f5c678d159e0f61
Steffield <steffilebby@gmail.com>
2020-02-12 09:59:20 -0500

    now all representatives load again

--- script.js
+++ script.js
@@ -231,31 +231,20 @@ function runRepresentativeQuery(queryURLRepresentatives){
             wellSection.addClass("card");
             wellSection.attr('id', 'repWell-'+i);
             $('#wellSection').append(wellSection);
-    
-            if(response.officials[i].photoUrl=== "null" || response.officials[i].photoUrl=== "undefined" ){
-                console.log(response.officials[i].name);
-                $("#repWell-"+i).append("<img src='http://placehold.it/128x128'>");
-    
-            }
 
-            //the urls throw errors bc some don't have any(I think), so I commented it out for now
-    
-            // if(response.officials[i].urls[0]=="null" || response.officials[i].urls[0]==="undefined"){
-            //     console.log(response.officials[i].name);
-            //     $("#repWell-"+i).append("<h4>No URL available</h4>");
-    
-            // }
+
+            var imgUrl = response.officials[i].photoUrl || "http://placehold.it/128x128";
+
     
             //Attach content to approp well
-            $("#repWell-"+i).append("<div class='card-image'><figure class='image is-128x128'><img src=" +response.officials[i].photoUrl+"></figure></div>");
+            $("#repWell-"+i).append("<div class='card-image'><figure class='image is-128x128'><img src=" +imgUrl+"></figure></div>");
             $("#repWell-"+i).append("<div class='card-content'><div class='content>");
             $("#repWell-"+i).append("<h4>Office: "+response.offices[i].name+"</h4>");
     
             $("#repWell-"+i).append("<h4>Name: "+response.officials[i].name+"</h4>");
             $("#repWell-"+i).append("<h4>Party: "+response.officials[i].party+"</h4>");
     
-            $("#repWell-"+i).append("<a href=" + response.officials[i].urls[0]+">"+response.officials[i].urls[0] +"</a>");
-            // var offUrl = (response.officials[i].urls && response.officials[i].urls[0]) ? response.officials[i].urls[0] : "no url available";
+            var offUrl = (response.officials[i].urls && response.officials[i].urls[0]) ? response.officials[i].urls[0] : "no url available";
 
             var offUrl;
 
